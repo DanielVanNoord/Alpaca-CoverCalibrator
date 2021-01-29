896d1ee00a6b4373a16cda7d89e9d20cbc63a01e
Daniel Van Noord <sg1mash98@gmail.com>
2021-01-29 14:16:51 -0500

    Added a catch all route to add an http 400 response to unused apis

---
+++ Alpaca.CoverCalibrator/Controllers/CatchAll.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Alpaca.CoverCalibrator.Controllers
+{
+    [ServiceFilter(typeof(AuthorizationFilter))]
+    [ApiController]
+    [ApiExplorerSettings(IgnoreApi = true)]
+    public class CatchAll : Controller
+    {
+        [HttpGet]
+        [Route("api", Order = 999)]
+        public async Task<IActionResult> CatchAllApiGets()
+        {
+            return BadRequest("Alpaca API route was not found on this driver");
+        }
+
+        [HttpPut]
+        [Route("api", Order = 999)]
+        public async Task<IActionResult> CatchAllApiPuts()
+        {
+            return BadRequest("Alpaca API route was not found on this driver");
+        }
+    }
+}
