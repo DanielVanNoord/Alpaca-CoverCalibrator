63f76e71185e59a6fe02d1dc8ea4af09400e2c5c
Daniel Van Noord <sg1mash98@gmail.com>
2021-03-05 19:29:22 -0500

    Added really basic publishing scripts

---
+++ Alpaca.CoverCalibrator/publish-windows-readytorun.bat
@@ -0,0 +1,2 @@
+dotnet publish -c Release -r win-x64 --self-contained true /p:PublishTrimmed=true /p:PublishReadyToRun=true /p:PublishReadyToRunShowWarnings=true -o ./bin/windows-x64
+dotnet publish -c Release -r win-x86 --self-contained true /p:PublishTrimmed=true /p:PublishReadyToRun=true /p:PublishReadyToRunShowWarnings=true -o ./bin/windows-x86
