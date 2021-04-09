90089b35666610bb2f783645b87f5b4c9fe55a68
Daniel Van Noord <sg1mash98@gmail.com>
2021-04-08 21:07:33 -0400

    Added tar command to Linux build script

--- Alpaca.CoverCalibrator/publish-linux-readytorun.bat
+++ Alpaca.CoverCalibrator/publish-linux-readytorun.bat
@@ -1,3 +1,9 @@
 dotnet publish -c Release -r linux-arm --self-contained true /p:PublishTrimmed=true /p:PublishReadyToRun=true /p:PublishReadyToRunShowWarnings=true -o ./bin/Alpaca.CoverCalibrator.linux-armhf
 dotnet publish -c Release -r linux-arm64 --self-contained true /p:PublishTrimmed=true /p:PublishReadyToRun=true /p:PublishReadyToRunShowWarnings=true -o ./bin/Alpaca.CoverCalibrator.linux-aarch64
 dotnet publish -c Release -r linux-x64 --self-contained true /p:PublishTrimmed=true /p:PublishReadyToRun=true /p:PublishReadyToRunShowWarnings=true -o ./bin/Alpaca.CoverCalibrator.linux-x64
+
+cd bin
+
+tar -cJf Alpaca.CoverCalibrator.linux-x64.tar.xz Alpaca.CoverCalibrator.linux-x64/
+tar -cJf Alpaca.CoverCalibrator.linux-aarch64.tar.xz Alpaca.CoverCalibrator.linux-aarch64/
+tar -cJf Alpaca.CoverCalibrator.linux-armhf.tar.xz Alpaca.CoverCalibrator.linux-armhf/
